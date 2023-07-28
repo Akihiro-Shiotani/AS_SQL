919aeb758de9103b3fd93457209090e5253d8cf4
Akihiro <a.shiotani.0904@gmail.com>
2023-07-28 10:16:32 +0900

    Fix: バージョンアップ
    機能修正、バージョンアップのため

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='AS_SQL',
-    version='1.0.0',
+    version='1.1.0',
     author='Akihiro-Shiotani',
     packages=find_packages(),
     install_requires=['requests'],
